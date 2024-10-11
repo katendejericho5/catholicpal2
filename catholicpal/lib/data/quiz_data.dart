1eaf2535a3ca77b7896c4b43a50f1bd27f9439a0
katendejericho <106674175+katendejericho5@users.noreply.github.com>
2024-10-11 10:03:34 +0300

    question quiz queations

--- catholicpal/lib/data/quiz_data.dart
+++ catholicpal/lib/data/quiz_data.dart
@@ -1,44 +1,132 @@
-// lib/data/question_data.dart
-
 import 'package:catholicpal/models/quiz/quiz_model.dart';
 
 List<Question> getQuestions() {
   return [
     Question(
       id: '1',
-      questionText: 'What is the capital of France?',
-      options: ['London', 'Berlin', 'Paris', 'Madrid'],
-      correctAnswerIndex: 2,
+      questionText: 'What is the first sacrament of initiation in the Catholic Church?',
+      options: ['Baptism', 'Eucharist', 'Confirmation', 'Reconciliation'],
+      correctAnswerIndex: 0,
     ),
     Question(
       id: '2',
-      questionText: 'Who painted the Mona Lisa?',
-      options: [
-        'Vincent van Gogh',
-        'Leonardo da Vinci',
-        'Pablo Picasso',
-        'Michelangelo'
-      ],
+      questionText: 'Which symbol is primarily used in the Sacrament of Baptism?',
+      options: ['Oil', 'Water', 'Fire', 'Bread'],
       correctAnswerIndex: 1,
     ),
     Question(
       id: '3',
-      questionText: 'What is the largest planet in our solar system?',
-      options: ['Mars', 'Jupiter', 'Saturn', 'Neptune'],
+      questionText: 'Baptism removes the stain of what?',
+      options: ['Venial sin', 'Original sin', 'Personal sin', 'All sin'],
       correctAnswerIndex: 1,
     ),
     Question(
       id: '4',
-      questionText: 'In which year did World War II end?',
-      options: ['1943', '1944', '1945', '1946'],
-      correctAnswerIndex: 2,
+      questionText: 'Which prayer is recited during a Catholic Baptism?',
+      options: ['Nicene Creed', 'Apostles’ Creed', 'Our Father', 'Hail Mary'],
+      correctAnswerIndex: 1,
     ),
     Question(
       id: '5',
-      questionText: 'What is the chemical symbol for gold?',
-      options: ['Go', 'Gd', 'Au', 'Ag'],
+      questionText: 'Who can administer the Sacrament of Baptism in case of an emergency?',
+      options: ['Only a priest', 'Only a deacon', 'Anyone', 'Only a bishop'],
       correctAnswerIndex: 2,
     ),
-    // Add more questions here...
+    Question(
+      id: '6',
+      questionText: 'What are the two main effects of Baptism?',
+      options: [
+        'Forgiveness of sin and becoming a member of the Church',
+        'Eternal life and receiving the Holy Spirit',
+        'Becoming a priest and eternal salvation',
+        'Forgiveness of all sins and receiving communion'
+      ],
+      correctAnswerIndex: 0,
+    ),
+    Question(
+      id: '7',
+      questionText: 'What do godparents pledge to do during a Baptism?',
+      options: [
+        'Pray for the baptized and help them grow in the faith',
+        'Financially support the child',
+        'Lead the child\'s education',
+        'Guide the child in career choices'
+      ],
+      correctAnswerIndex: 0,
+    ),
+    Question(
+      id: '8',
+      questionText: 'In which part of the Bible is Jesus baptized by John?',
+      options: ['Matthew', 'Mark', 'Luke', 'John'],
+      correctAnswerIndex: 0,
+    ),
+    Question(
+      id: '9',
+      questionText: 'Which form of Baptism involves pouring water on the person’s head?',
+      options: ['Immersion', 'Aspersion', 'Affusion', 'Submersion'],
+      correctAnswerIndex: 2,
+    ),
+    Question(
+      id: '10',
+      questionText: 'Baptism is a requirement for entry into which of the following?',
+      options: ['Heaven', 'Priesthood', 'Church membership', 'Christian life'],
+      correctAnswerIndex: 3,
+    ),
+    Question(
+      id: '11',
+      questionText: 'What is the Trinitarian formula used in Baptism?',
+      options: [
+        'In the name of Jesus Christ',
+        'In the name of the Father, the Son, and the Holy Spirit',
+        'In the name of the Church',
+        'In the name of Mary and the Saints'
+      ],
+      correctAnswerIndex: 1,
+    ),
+    Question(
+      id: '12',
+      questionText: 'Which Church council reaffirmed the necessity of Baptism for salvation?',
+      options: [
+        'Council of Trent',
+        'First Vatican Council',
+        'Council of Nicaea',
+        'Second Vatican Council'
+      ],
+      correctAnswerIndex: 0,
+    ),
+    Question(
+      id: '13',
+      questionText: 'What do the white garment and candle symbolize in Baptism?',
+      options: [
+        'Victory over sin and eternal life',
+        'Purity and the light of Christ',
+        'Salvation and resurrection',
+        'Faith and hope'
+      ],
+      correctAnswerIndex: 1,
+    ),
+    Question(
+      id: '14',
+      questionText: 'What does Baptism signify spiritually?',
+      options: [
+        'Entering into Christ’s death and resurrection',
+        'Becoming an ordained minister',
+        'Completion of faith',
+        'Purification of the body'
+      ],
+      correctAnswerIndex: 0,
+    ),
+    Question(
+      id: '15',
+      questionText: 'Which oil is used in the anointing during Baptism?',
+      options: [
+        'Chrism oil',
+        'Olive oil',
+        'Oil of catechumens',
+        'Oil of salvation'
+      ],
+      correctAnswerIndex: 2,
+    ),
+    // Add more questions here if needed...
   ];
 }
--- catholicpal/lib/screens/home/sacraments/baptism/bible_quiz/quiz_results_page.dart
+++ catholicpal/lib/screens/home/sacraments/baptism/bible_quiz/quiz_results_page.dart
@@ -1,4 +1,5 @@
 import 'package:catholicpal/providers/quiz_provider.dart';
+import 'package:catholicpal/screens/home/home_page.dart';
 import 'package:catholicpal/screens/home/sacraments/baptism/bible_quiz/quiz_questions.dart';
 import 'package:catholicpal/screens/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
@@ -31,123 +32,149 @@ class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
     final quizProvider = Provider.of<QuizProvider>(context);
-    final theme = Theme.of(context); // Accessing the theme
+    final theme = Theme.of(context);
     double scorePercentage = quizProvider.score / quizProvider.totalQuestions;
+
+    // Define progress color logic
+    Color getProgressColor() {
+      if (scorePercentage > 0.6) {
+        return Colors.greenAccent; // High score
+      } else if (scorePercentage >= 0.4) {
+        return Colors.amber; // Average score
+      } else {
+        return Colors.redAccent; // Low score
+      }
+    }
 
     return Scaffold(
       appBar: CustomAppBar(
         title: 'Quiz Results',
         scrollController: _scrollController,
-        actions: const [],
+        actions: const [], // Removed back button actions
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Circular Score Indicator
-              CircularPercentIndicator(
-                radius: 120.0,
-                lineWidth: 15.0,
-                percent: scorePercentage,
-                center: Text(
-                  '${(scorePercentage * 100).toStringAsFixed(0)}%',
-                  style: GoogleFonts.poppins(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: scorePercentage > 0.6
-                        ? theme.colorScheme.primary
-                        : theme.colorScheme
-                            .error, // Theme colors based on performance
-                  ),
-                ),
-                progressColor: scorePercentage > 0.6
-                    ? theme.colorScheme.primary
-                    : theme.colorScheme.error,
-                backgroundColor: theme.colorScheme.onSurface.withOpacity(0.2),
-                circularStrokeCap: CircularStrokeCap.round,
-                animation: true,
-                animationDuration: 1200,
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Center(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
               ),
-              const SizedBox(height: 40),
+              elevation: 10,
+              shadowColor: Colors.black45,
+              color: theme.colorScheme.surface,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0, vertical: 16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Circular Score Indicator
+                    CircularPercentIndicator(
+                      radius: 120.0,
+                      lineWidth: 15.0,
+                      percent: scorePercentage,
+                      center: Text(
+                        '${(scorePercentage * 100).toStringAsFixed(0)}%',
+                        style: GoogleFonts.poppins(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: getProgressColor(), // Feedback color
+                        ),
+                      ),
+                      progressColor: getProgressColor(),
+                      backgroundColor:
+                          theme.colorScheme.onSurface.withOpacity(0.2),
+                      circularStrokeCap: CircularStrokeCap.round,
+                      animation: true,
+                      animationDuration: 1200,
+                    ),
+                    const SizedBox(height: 40),
 
-              // "Quiz Completed" Text
-              Text(
-                'Quiz Completed!',
-                style: GoogleFonts.poppins(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: theme
-                      .textTheme.titleLarge!.color, // Using theme's text color
+                    // "Quiz Completed" Text
+                    Text(
+                      'Quiz Completed!',
+                      style: GoogleFonts.poppins(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: theme.textTheme.titleLarge?.color,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Display Final Score
+                    Text(
+                      'Your Score: ${quizProvider.score}/${quizProvider.totalQuestions}',
+                      style: GoogleFonts.poppins(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500,
+                        color: theme.textTheme.bodyLarge?.color,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+
+                    // Restart Quiz and Back to Home in the same row
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // Restart Button with Icon
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            quizProvider.resetQuiz();
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => const QuizPage()),
+                            );
+                          },
+                          // HomePage
+                          icon: const Icon(Icons.restart_alt), // Restart icon
+                          label: Text(
+                            'Restart Quiz',
+                            style: GoogleFonts.poppins(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              color: theme.colorScheme.onPrimary,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 32, vertical: 16),
+                            backgroundColor: theme.colorScheme.primary,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                          ),
+                        ),
+
+                        // Back to Home Button
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => const HomePage(),
+                              ),
+                            );
+                            quizProvider.resetQuiz();
+                          },
+                          child: Text(
+                            'Back to Home',
+                            style: GoogleFonts.poppins(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 20),
-
-              // Display Final Score
-              Text(
-                'Your Score: ${quizProvider.score}/${quizProvider.totalQuestions}',
-                style: GoogleFonts.poppins(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w500,
-                  color: theme
-                      .textTheme.bodyLarge!.color, // Body text color from theme
-                ),
-              ),
-              const SizedBox(height: 30),
-
-              // Restart Button with Icon
-              ElevatedButton.icon(
-                onPressed: () {
-                  quizProvider.resetQuiz();
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => const QuizPage()),
-                  );
-                },
-                icon: const Icon(Icons.restart_alt), // Icon for the restart
-                label: Text(
-                  'Restart Quiz',
-                  style: GoogleFonts.poppins(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: theme.colorScheme.onPrimary,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                  textStyle: GoogleFonts.poppins(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: theme.colorScheme.onPrimary, // Text color on button
-                  ),
-                  backgroundColor:
-                      theme.colorScheme.primary, // Button background color
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              // Option to go back to home or other sections
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .pop(); // Navigates back to the home page
-                },
-                child: Text(
-                  'Back to Home',
-                  style: GoogleFonts.poppins(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: theme
-                        .colorScheme.primary, // Primary color for back button
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
